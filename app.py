f3455d22503167ca5046fb6bc97373ade42f3db7
bvcvbcvncxzcvzxvc <132753232+bvcvbcvncxzcvzxvc@users.noreply.github.com>
2025-04-03 09:40:47 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/send', methods=['POST'])
+def send():
+    data = request.json
+    message = data.get("message")
+
+    # فقط درخواست‌های معتبر رو قبول می‌کنیم
+    if message == "Get EagleSpy-V5 link":
+        return jsonify({"response": "Get EagleSpy-V5 link Link: https://t.me/c/2344120391/214/233"})
+    elif message == "Get CraxsRat-7.6 link":
+        return jsonify({"response": "Get CraxsRat-7.6 link Link: https://t.me/c/2267427894/620"})
+    else:
+        return jsonify({"error": "Invalid request"}), 400
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
